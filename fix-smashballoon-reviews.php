330afcb2bd360b30f6b685e0ac989fe9e52ba953
nexTab - Oliver Gehrmann <info@nextab.de>
2026-03-01 11:22:57 +0100

    Settings-Link for Plugin Overview

--- fix-smashballoon-reviews.php
+++ fix-smashballoon-reviews.php
@@ -19,8 +19,15 @@ define( 'FSBR_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 require_once FSBR_PLUGIN_DIR . 'includes/class-sbr-cache-sync.php';
 
 add_action( 'init', 'fsbr_init' );
+add_filter( 'plugin_action_links_' . FSBR_PLUGIN_BASENAME, 'fsbr_plugin_action_links' );
 register_activation_hook( __FILE__, 'fsbr_activate' );
 register_deactivation_hook( __FILE__, 'fsbr_deactivate' );
+
+function fsbr_plugin_action_links( $links ) {
+	$url = admin_url( 'options-general.php?page=fsbr-cache-sync' );
+	$links[] = '<a href="' . esc_url( $url ) . '">' . esc_html__( 'Einstellungen', 'fix-smashballoon-reviews' ) . '</a>';
+	return $links;
+}
 
 function fsbr_init() {
 	$sync = new SBR_Cache_Sync();
